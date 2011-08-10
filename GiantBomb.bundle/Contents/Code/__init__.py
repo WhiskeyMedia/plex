03793ae69ea00e524c4c58efb00d1afe3bc3c56b
sdornan <sam@whiskeymedia.com>
2011-08-10 16:13:44 -0700

    Branch for not-live-yet API changes

--- GiantBomb.bundle/Contents/Code/__init__.py
+++ GiantBomb.bundle/Contents/Code/__init__.py
@@ -1,10 +1,24 @@
-API_KEY = 'e5529a761ee3394ffbd237269966e9f53a4c7bf3'
+API_PATH = 'http://localhost:8000'
+# API_KEY = 'e5529a761ee3394ffbd237269966e9f53a4c7bf3' # Default API key
+API_KEY = '3353202b4c9af87523cab1184e471fd1c5be4def'
 
 ART = 'art-default.png'
 ICON = 'icon-default.png'
 
+def ValidatePrefs():
+    access_code = Prefs['access_code']
+    if access_code != '' and access_code != Dict['access_code']:
+        response = JSON.ObjectFromURL(API_PATH + '/validate?access_code=' + access_code + '&format=json')
+        if api_key in response:
+            Dict['api_key'] = response['api_key']
+            Dict['access_code'] = access_code
+
 @handler('/video/giantbomb', 'Giant Bomb')
 def MainMenu():
+    if 'api_key' in Dict:
+        global API_KEY
+        API_KEY = Dict['api_key']
+
     oc = ObjectContainer()
 
     live_stream = JSON.ObjectFromURL('http://api.justin.tv/api/stream/list.json?channel=whiskeymedia')
@@ -23,22 +37,21 @@ def MainMenu():
 
     oc.add(
         DirectoryObject(
-            key=Callback(Videos),
+            key='/video/giantbomb/videos',
             title='Latest Videos',
             summary='Watch the newest stuff.',
             thumb=R(ICON),
             art=R(ART)
-
         )
     )
 
-    categories = JSON.ObjectFromURL('http://api.giantbomb.com/video_types/?api_key=' + API_KEY + '&format=json')['results']
+    categories = JSON.ObjectFromURL(API_PATH + '/video_types/?api_key=' + API_KEY + '&format=json')['results']
 
     for cat in categories:
         if cat['id'] == 5:
             oc.add(
                 DirectoryObject(
-                    key=Callback(EnduranceRunMenu),
+                    key='/video/giantbomb/erun',
                     title=cat['name'],
                     summary=cat['deck'],
                     thumb=R(ICON),
@@ -48,7 +61,7 @@ def MainMenu():
         else:
             oc.add(
                 DirectoryObject(
-                    key=Callback(Videos, cat_id=str(cat['id'])),
+                    key='/video/giantbomb/videos/?cat_id=' + str(cat['id']),
                     title=cat['name'],
                     summary=cat['deck'],
                     thumb=R(ICON),
@@ -76,21 +89,22 @@ def MainMenu():
 
     return oc
 
+@route('/video/giantbomb/erun')
 def EnduranceRunMenu():
     oc = ObjectContainer(
         objects = [
             DirectoryObject(
-                key=Callback(Videos, cat_id='5-DP'),
+                key='/video/giantbomb/videos/?cat_id=5-DP',
                 title='Deadly Premonition',
                 art=R(ART)
             ),
             DirectoryObject(
-                key=Callback(Videos, cat_id='5-P4'),
+                key='/video/giantbomb/videos/?cat_id=5-P4',
                 title='Persona 4',
                 art=R(ART)
             ),
             DirectoryObject(
-                key=Callback(Videos, cat_id='5-MO'),
+                key='/video/giantbomb/videos/?cat_id=5-MO',
                 title='The Matrix Online: Not Like This',
                 art=R(ART)
             )
@@ -99,39 +113,48 @@ def EnduranceRunMenu():
 
     return oc
 
+@route('/video/giantbomb/videos')
 def Videos(cat_id=None, query=None):
     oc = ObjectContainer()
 
     if query:
-        videos = JSON.ObjectFromURL('http://api.giantbomb.com/search/?api_key=' + API_KEY + '&resources=video&query=' + query + '&format=json')['results']
+        videos = JSON.ObjectFromURL(API_PATH + '/search/?api_key=' + API_KEY + '&resources=video&query=' + query + '&format=json')['results']
     elif cat_id == '5-DP':
-        videos = JSON.ObjectFromURL('http://api.giantbomb.com/videos/?api_key=' + API_KEY + '&video_type=5&offset=161&format=json')['results']
+        videos = JSON.ObjectFromURL(API_PATH + '/videos/?api_key=' + API_KEY + '&video_type=5&offset=161&format=json')['results']
     elif cat_id == '5-P4':
-        videos = JSON.ObjectFromURL('http://api.giantbomb.com/videos/?api_key=' + API_KEY + '&video_type=5&format=json')['results']
-        videos += JSON.ObjectFromURL('http://api.giantbomb.com/videos/?api_key=' + API_KEY + '&video_type=5&offset=100&limit=61&format=json')['results']
+        videos = JSON.ObjectFromURL(API_PATH + '/videos/?api_key=' + API_KEY + '&video_type=5&format=json')['results']
+        videos += JSON.ObjectFromURL(API_PATH + '/videos/?api_key=' + API_KEY + '&video_type=5&offset=100&limit=61&format=json')['results']
         videos = [video for video in videos if not video['name'].startswith('The Matrix Online')]
     elif cat_id == '5-MO':
-        videos = JSON.ObjectFromURL('http://api.giantbomb.com/videos/?api_key=' + API_KEY + '&video_type=5&offset=105&limit=21&format=json')['results']
+        videos = JSON.ObjectFromURL(API_PATH + '/videos/?api_key=' + API_KEY + '&video_type=5&offset=105&limit=21&format=json')['results']
         videos = [video for video in videos if video['name'].startswith('The Matrix Online')]
     elif cat_id:
-        videos = JSON.ObjectFromURL('http://api.giantbomb.com/videos/?api_key=' + API_KEY + '&video_type=' + cat_id + '&sort=-publish_date&format=json')['results']
+        videos = JSON.ObjectFromURL(API_PATH + '/videos/?api_key=' + API_KEY + '&video_type=' + cat_id + '&sort=-publish_date&format=json')['results']
     else:
-        videos = JSON.ObjectFromURL('http://api.giantbomb.com/videos/?api_key=' + API_KEY + '&sort=-publish_date&format=json')['results']
+        videos = JSON.ObjectFromURL(API_PATH + '/videos/?api_key=' + API_KEY + '&sort=-publish_date&format=json')['results']
 
-    if Prefs['quality'] == 'Low':
-        quality = '_700.mp4'
+    if Prefs['quality'] == 'Auto':
+        if 'hd_url' in videos[0]:
+            quality = 'hd_url'
+        else:
+            quality = 'high_url'
     else:
-        quality = '_1500.mp4'
+        quality = Prefs['quality'].lower() + '_url'
 
     for vid in videos:
-        if 'wallpaper_image' not in vid or not vid['wallpaper_image']: #or whatever it gets called
+        if 'wallpaper_image' not in vid or not vid['wallpaper_image']: # or whatever it gets called
             vid_art = R(ART)
         else:
             vid_art = vid['wallpaper_image']
 
+        if quality == 'hd_url':
+            url = vid[quality] + '?api_key=' + API_KEY
+        else:
+            url = vid[quality]
+
         oc.add(
                 VideoClipObject(
-                    key='http://media.giantbomb.com/video/' + vid['url'].replace('.mp4', quality),
+                    key=url,
                     title=vid['name'],
                     summary=vid['deck'],
                     thumb=vid['image']['screen_url'],
@@ -140,4 +163,3 @@ def Videos(cat_id=None, query=None):
         )
 
     return oc
-
